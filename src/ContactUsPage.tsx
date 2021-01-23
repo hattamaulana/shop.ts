4d7c2c0302fc7c84d1abf5050519fa3675edd7fa
hattamaulana <mahatta.maulana@gmail.com>
2021-01-23 08:21:37 +0700

    complete form

--- src/ContactUsPage.tsx
+++ src/ContactUsPage.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import ContactUs from './ContactUs';
+import { ISubmitResult, IValues } from "./Form";
 
 interface IState {
     name: string;
@@ -10,29 +11,17 @@ interface IState {
 };
 
 class ContactUsPage extends React.Component<{}, IState> {
-    private handleNameChange = (name: string) => {
-        this.setState({ name });
-    };
+    private handleSubmit = async (values: IValues): Promise<ISubmitResult> => {
+        const wait = (ms: number): Promise<void> => {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
 
-    private handleEmailChange = (email: string) => {
-        this.setState({ email });
-    };
-
-    private handleReasonChange = (reason: string) => {
-        this.setState({ reason });
-    };
-
-    private handleNotesChange = (notes: string) => {
-        this.setState({ notes });
-    };
-
-    public constructor(props: {}) {
-        super(props);
-        this.state = {
-            email: "",
-            name: '',
-            reason: '',
-            notes: ''
+        await wait(1000);
+        return {
+            errors: {
+                email: ['Some is wrong with this']
+            },
+            success: false
         }
     }
 
@@ -44,16 +33,7 @@ class ContactUsPage extends React.Component<{}, IState> {
                     If you enter your details we'll get back to you as soon as we can.
                 </p>
 
-                <ContactUs
-                 name={this.state.name}
-                 onNameChange={this.handleNameChange}
-                 email={this.state.email}
-                 onEmailChange={this.handleEmailChange}
-                 reason={this.state.reason}
-                 onReasonChange={this.handleReasonChange}
-                 notes={this.state.notes}
-                 onNotesChange={this.handleNotesChange}
-                 />
+                <ContactUs onSubmit={ this.handleSubmit } />
             </div>
         );
     }
